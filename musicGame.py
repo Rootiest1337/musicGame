1b089c1977d32d1c2f473e0d5acfeb7136fbabb2
Josh Armstrong <josh.armstrong.1337@gmail.com>
2021-03-06 19:18:45 +0000

    Uploading final product, as well as updating databases

--- musicGame.py
+++ musicGame.py
@@ -8,10 +8,10 @@ musiclist = []
 scoreboard = []
 limit = 0
 points = 0
-lives = 3
+lives = 2
 current_user = ""
 
-# FUNCTION DEFINITION
+# FUNCTION DEFINITIONS
 
 # Pulls the details of the music from musicDB.txt
 def loadmusic():
@@ -54,7 +54,7 @@ def gen_scoreboard():
         while c < max_scores:
             print(str(c +1) + ". " + scores[c][0] + " - " + str(scores[c][1]))
             c += 1
-        wait = input("\n\nPress RETURN to continue")
+        input("\n\nPress RETURN to continue")
         return
     else: print("No High Scores yet! Get in there and makes some!")
     wait = input("\n\nPress RETURN to continue")
@@ -69,59 +69,82 @@ def quiz():
     answer = " "
     while play.lower() != "n":
         quiz_no = random.randint(0, limit - 1)
-        letters = gen_question(quiz_no)
-        letter_no = len(letters)
-        question = "\nWhat song by " + musiclist[quiz_no][1] + " starts with the letter"
-        if letter_no > 1:
-            question = question + "s " 
-            c = 0
-            while c < letter_no:
-                if c < (letter_no - 2):
-                    question = question + str(letters[c]) + ", "
-                elif c < (letter_no - 1):
-                    question = question + str(letters[c]) + " "
-                elif c == letter_no - 1:
-                        question = question +  "and " + str(letters[c]) + "?"
-                c = c + 1
-        else:
-            question  = question +  " " + str(letters[0]) + "?"
-        while answer != "quit":   
+        question = gen_question(quiz_no)
+        lives = 2
+        score = 0
+        answer = ""
+        while answer != "pass":   
             print(question)
             print ("HINT: (Answer is '"+ musiclist[quiz_no][0] + "')")
             print("\nYou Currently have " + str(lives) + " attempts at this question! Your current score is " + str(points) + "!")
-            answer = input("Answer : ")
-            check_answer(answer, quiz_no)
+            answer = input("Answer : ").lower()
+            score = check_answer(answer, quiz_no)
+            points = score + points
+            if score > 0:
+                break
             if lives == 0:
                 game_over()
                 break
         play = input("\nDo you want to play again? (Y/N) : ")
+    game_over()
+        
 
-# Breaks the Answer down into Letters
+# Creates Question
 def gen_question(quiz_no):
     global musiclist
-    question = []
+    letters = []
     word_split = musiclist[quiz_no][0].split(" ")
     word_limit = len(word_split)
     c = 0
     while c < word_limit:
          first_letter = list(word_split[c])
-         question.append(first_letter[0])
+         letters.append(first_letter[0])
          c = c + 1
+    letter_no = len(letters)
+    question = "\nWhat song by " + musiclist[quiz_no][1] + " starts with the letter"
+    if letter_no > 1:
+        question = question + "s " 
+        c = 0
+        while c < letter_no:
+            if c < (letter_no - 2):
+                question = question + str(letters[c]) + ", "
+            elif c < (letter_no - 1):
+                question = question + str(letters[c]) + " "
+            elif c == letter_no - 1:
+                    question = question +  "and " + str(letters[c]) + "?"
+            c = c + 1
+    else:
+        question  = question +  " " + str(letters[0]) + "?"
     return question
 
 # Check the Answer of the question
 def check_answer(answer, quiz_no):
     global musiclist
     global lives
+    points = 0
     correct_answer = musiclist[quiz_no][0].lower()
+    if answer == correct_answer:
+        if lives == 2:
+            points = 3
+        elif lives == 1:
+            points = 1
+        print("\nCORRECT! Well done, you scored " + str(points) + " more points!")
+        return(points)
+    else:
+        lives = lives - 1
+        print("\nINCORRECT! Try again!")
+        return(points)
+
+
 
 # Game over process
 def game_over():
     global points
     global lives
+    global current_user
     if lives == 0:
         print("\nOh No! You ran out of lives! Sorry but that is game over!")
-    print("\nYou managed to score " + str(points) + " points!")
+    print("\nYou managed to score a total of " + str(points) + " points!")
     if points == 0:
         print("Get Gud Noob!")
     elif points < 4:
@@ -130,7 +153,11 @@ def game_over():
         print("Good going!")
     elif points >= 10:
         print("Wow! You did really well!")
-
+    print("\n How did you do? This is our current High Scores")
+    gen_scoreboard()
+    if points > 0:
+        scoreboard.append([current_user, points])
+    
 # Menu
 def menu():
     global current_user
@@ -149,6 +176,19 @@ def menu():
 
 # Ends the game and saves the scoreboard to file
 def end_game():
+    global scoreboard
+    c = 0
+    c_max = len(scoreboard)
+    string = ""
+    while c < c_max:
+        string = string + str(scoreboard[c][0]) + "," + str(scoreboard[c][1])
+        if c < c_max -1:
+            string = string + "/"
+        c = c + 1
+    print("\n\nFile Save Format = " + string)
+    f = open("pointsDB.txt", "w")
+    f.write(string)
+    f.close()
     exit()
 
 
